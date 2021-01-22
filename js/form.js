69cf136acd8c04e48689d6361327e3cdbc5ba36d
ShannonDias <74082043+ShannonDias@users.noreply.github.com>
2021-01-22 15:16:31 +1300

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,26 @@
+class Form{
+   constructor(){
+
+   }
+   display(){
+     var title = createElement('h2')
+     title.html ('Car Racing Game!')
+     title.position(130,0)
+     var input = createInput('name')
+     var button = createButton('start')
+     input.position (130,160)
+     button.position(250,200)
+     var greeting = createElement('h3')
+     button.mousePressed(function(){
+         input.hide()
+         button.hide()
+         var name = input.value()
+         playerCount +=1
+         player.update(name)
+         player.updateCount(playerCount)
+         greeting.html('Good luck ' + name)
+         greeting.position(130,160)
+     })
+     
+   }
+}
